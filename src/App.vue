da56248bd7d1fcc93c6b14a2c1efe7adf392ce13
MiaoMint <1981324730@qq.com>
2022-10-24 10:50:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
+<style scoped lang='scss'>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="muyu">
+
+    </div>
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  .muyu {
+    background-image: url("/img/muyu.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 100%;
+    width: 200px;
+    height: 200px;
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+    &:hover {
+      transform: scale(1.4)
+    }
+
+    &:active{
+      transform: scale(0.4)
+    }
+  }
+
+}
+</style>
