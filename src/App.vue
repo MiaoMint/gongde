0ff83b80bb92e773f81383ac8a74bc7819ba2ec8
MiaoMint <1981324730@qq.com>
2023-09-23 14:10:54 +0800

    Chrome extesnion

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,58 @@
 <script setup lang="ts">
 import { ref } from "vue";
-const items = ref<Array<{ id: number, text: String }>>([])
-const count = ref<number>(Number(localStorage.getItem("gongde")))
-const fozuText = ref<string>("扣1佛祖陪你笑")
-
+const items = ref<Array<{ id: number; text: String }>>([]);
+const count = ref<number>(Number(localStorage.getItem("gongde")));
+const fozuText = ref<string>("扣1佛祖陪你笑");
+const isPopup = chrome.extension !== undefined;
 
 const danmaku = (text: string) => {
-  items.value.push({ id: Math.random(), text })
+  items.value.push({ id: Math.random(), text });
   setTimeout(() => {
-    items.value.shift()
-  }, 300)
-}
+    items.value.shift();
+  }, 300);
+};
 
 const play = () => {
-  const muyuVoice = new Audio('/mp3/muyu.mp3');
-  localStorage.setItem("gongde", String(++count.value))
+  const muyuVoice = new Audio("/mp3/muyu.ogg");
+  localStorage.setItem("gongde", String(++count.value));
   muyuVoice.play();
-  danmaku("功德+1")
-}
+  danmaku("功德+1");
+};
 
 const laugh = () => {
-  const fozuVoice = new Audio('/mp3/fozu.wav')
+  const fozuVoice = new Audio("/mp3/fozu.ogg");
   if (Number(localStorage.getItem("gongde")) >= 10) {
-    count.value -= 10
-    localStorage.setItem("gongde", String(count.value))
-    danmaku("功德-10")
+    count.value -= 10;
+    localStorage.setItem("gongde", String(count.value));
+    danmaku("功德-10");
     fozuVoice.play();
   } else {
-    fozuText.value = "功德不足"
-    setTimeout(() => (fozuText.value = "扣1佛祖陪你笑"), 2000)
+    fozuText.value = "功德不足";
+    setTimeout(() => (fozuText.value = "扣1佛祖陪你笑"), 2000);
   }
-}
+};
 
-document.addEventListener('keypress', (event) => {
-  if (event.code === "Digit1") laugh()
-})
+document.addEventListener("keypress", (event) => {
+  if (event.code === "Digit1") laugh();
+});
 </script>
 
 <template>
-  <main>
+  <main :class="{ popup: isPopup }">
     <div class="plus">
       <TransitionGroup tag="ul" name="list">
-        <li style="color: #e6b422" v-for="(item, index) in items" :key="item.id" :data-index="index">
+        <li
+          style="color: #e6b422"
+          v-for="(item, index) in items"
+          :key="item.id"
+          :data-index="index"
+        >
           {{ item.text }}
         </li>
       </TransitionGroup>
     </div>
     <div class="muyu" @mousedown="play"></div>
-    <div class="text">
-      功德：{{ count }}
-    </div>
+    <div class="text">功德：{{ count }}</div>
     <div class="text" @mousedown="laugh">
       {{ fozuText }}
     </div>
@@ -57,6 +60,11 @@ document.addEventListener('keypress', (event) => {
 </template>
 
 <style scoped lang="scss">
+.popup {
+  width: 300px;
+  height: 500px;
+}
+
 .list-enter-active,
 .list-leave-active {
   transition: all 0.3s ease;
@@ -98,14 +106,14 @@ main {
     background-size: 100%;
     width: 300px;
     height: 300px;
-    transition: .5s cubic-bezier(0.075, 0.82, 0.165, 1);
+    transition: 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
 
     &:hover {
-      transform: scale(1.2)
+      transform: scale(1.2);
     }
 
     &:active {
-      transform: scale(0.6)
+      transform: scale(0.6);
     }
   }
 
@@ -114,6 +122,5 @@ main {
     font-weight: bold;
     margin-bottom: 1em;
   }
-
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./manifest.json",
+          dest: "./",
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
